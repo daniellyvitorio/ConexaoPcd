9aa3d5e8ac875f0aebb702d5205d5eeb79b96704
danielly vitorio <danielly_vitorio@estudante.sesisenai.org.br>
2024-06-24 22:02:13 -0300

    aula dia 24-06

--- src/main/java/com/example/demo/Model/Endereco.java
+++ src/main/java/com/example/demo/Model/Endereco.java
@@ -1,74 +1,74 @@
 package com.example.demo.Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
 
 @Entity
-@Table(name = "endereco")
-public class Endereco {
-
+@Table(
+        name = "endereco"
+)
+public class Endereco implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
     private Long id;
-
-    @Column(name = "logadouro", nullable = false)
-    private String logadouro;
-
-    @Column(name = "numero", nullable = false)
-    private Double  numero;
-
-    @Column(name = "cep", nullable = false)
-    private Double cep;
+    @Column(
+            nullable = false
+    )
+    private String logradouro;
+    @Column(
+            nullable = false
+    )
+    private String cep;
+    @Column(
+            nullable = true
+    )
+    private int numero;
 
     @ManyToOne
-    @JoinColumn(name = "bairro_id", nullable = false)
     private Bairro bairro;
+ ;
+
+    public Endereco() {
+    }
 
     public Long getId() {
         return this.id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getLogradouro() {
+        return this.logradouro;
     }
 
-    public String getLogadouro() {
-        return this.logadouro;
-    }
-
-    public void setLogadouro(String logadouro) {
-        this.logadouro = logadouro;
-    }
-
-    public Double getNumero() {
-        return this.numero;
-    }
-
-    public void setNumero(Double numero) {
-        this.numero = numero;
-    }
-
-    public Double getCep() {
+    public String getCep() {
         return this.cep;
     }
 
-    public void setCep(Double cep) {
+    public int getNumero() {
+        return this.numero;
+    }
+
+
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public void setLogradouro(final String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setCep(final String cep) {
         this.cep = cep;
     }
 
-    public Bairro getBairro() {
-        return bairro;
+    public void setNumero(final int numero) {
+        this.numero = numero;
     }
 
-    public void setBairro(Bairro bairro) {
-        this.bairro = bairro;
-    }
+
 
 }
--- src/main/java/com/example/demo/Model/Usuario.java
+++ src/main/java/com/example/demo/Model/Usuario.java
@@ -1,107 +1,41 @@
 package com.example.demo.Model;
 
+import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.List;
 
 @Entity
-@Table(name = "usuario")
+@Table(name ="usuario")
 public class Usuario {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
-    
-    @Column(name = "cpf", nullable = false, length = 14)
+    private long id;
+
+    @Column(name = "cpf", nullable = false,length = 14)
     private String cpf;
     
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false,length = 100)
     private String email;
     
-    @Column(name = "senha", nullable = false, length = 256)
+    @Column(name = "senha", nullable = false,length = 50)
     private String senha;
 
     @ManyToMany
-    @JoinTable(name = "usuario_nivel_acesso", joinColumns = @JoinColumn(name = "usuario_id"),
-        inverseJoinColumns = @JoinColumn(name = "nivel_acesso_id")
-        )
-    private List<NivelAcesso > nivelAcesso;
+    private List<NivelAcesso> nivelAcesso;
 
-   @OneToMany(mappedBy = "usuario")
-   private List<Noticia> noticia;
-
-   public Usuario(List<NivelAcesso> nivelAcesso, List<Noticia> noticia) {
-    this.nivelAcesso = nivelAcesso;
-    this.noticia = noticia;
-}
-
-@OneToMany(mappedBy = "usuario")
-    private List<Acesso> acesso;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCpf() {
-        return this.cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return this.senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public List<NivelAcesso> getNivelAcesso() {
-        return nivelAcesso;
-    }
-
-    public void setNivelAcesso(List<NivelAcesso> nivelAcesso) {
-        this.nivelAcesso = nivelAcesso;
-    }
-
-    public List<Noticia> getNoticia() {
-        return noticia;
-    }
-
-    public void setNoticia(List<Noticia> noticia) {
-        this.noticia = noticia;
-    }
-
-    public List<Acesso> getAcesso() {
-        return acesso;
-    }
-
-    public void setAcesso(List<Acesso> acesso) {
-        this.acesso = acesso;
-    }
-
+    @Column(name = "ativo",nullable = true)
+    private boolean ativo;
     
 }
+
+
+
+
+    
